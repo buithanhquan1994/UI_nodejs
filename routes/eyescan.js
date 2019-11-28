028d6ca2b3243d7d92e0f06ccadf53edba700d5a
buithanhquan1994 <mualua94@gmail.com>
2019-11-28 09:25:21 +0700

    Add Flash module

--- routes/eyescan.js
+++ routes/eyescan.js
@@ -9,9 +9,31 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
-// eyescan/
+// eyescan/index
 router.get('/',(req, res) => {
+  res.render('eyescan/index');
+});
 
+// eyescan/detail
+router.get('/detail/:image_id',(req, res) => {
+    res.send(req.params.image_id);
+
+});
+
+// form post search
+router.post('/search',(req,res) => {
+  var valInput = req.body.keywordInput.replace(' ','');
+  var keyword = valInput.split(',');
+  keyword = Object.assign({}, keyword);
+
+  res.redirect('/eyescan/search/'+JSON.stringify(keyword));
+});
+
+// view result search
+router.get('/search/:keyword',(req,res) => {
+  var keyword = JSON.parse(req.params.keyword);
+
+  // FAKE DATA
   var objectImage = {
     listImg: {
 
@@ -71,42 +93,12 @@ router.get('/',(req, res) => {
 
       }
     };
-
-
-
-
-  var objectKey = Object.keys(objectImage.listImg);
-  res.render('eyescan/index',{
+    var objectKey = Object.keys(objectImage.listImg);
+    res.render('eyescan/search',{
+              keywordObj : keyword,
               listIMG: objectImage.listImg,
               objectKey: objectKey,
-              anterior: objectImage.listImg.Anterior,
-              posterior : objectImage.listImg.Posterior,
-              other: objectImage.listImg.Other,
-
-            });
-  console.log(objectImage);
-});
-
-
-router.get('/detail/:image_id',(req, res) => {
-    res.send(req.params.image_id);
-
-});
-
-router.post('/search',(req,res) => {
-  var valInput = req.body.keywordInput.replace(' ','');
-  var keyword = valInput.split(',');
-  keyword = Object.assign({}, keyword);
-  
-  res.redirect('/eyescan/search/'+JSON.stringify(keyword));
-});
-
-router.get('/search/:keyword',(req,res) => { 
-  var keyword = JSON.parse(req.params.keyword);
-  res.render('eyescan/search',{
-            keywordObj : keyword
-
-  }); 
+  });
 });
 
 module.exports = router;
